a80f4d59c2bf9d00b5a4588e68cb2b9029efd0b1
herrmannplatz <rene@herrmannplatz.de>
2020-03-10 19:17:15 +0100

    chore(package): update semantic-release and standard

--- generator/index.js
+++ generator/index.js
@@ -1,7 +1,7 @@
 module.exports = (api, options, rootOptions) => {
   api.extendPackage({
     scripts: {
-      'docker': 'docker build . -t app && docker run -d -p 8080:8080 app'
+      docker: 'docker build . -t app && docker run -d -p 8080:8080 app'
     }
   })
 
